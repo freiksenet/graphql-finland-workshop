e95381f33fb882c79d0fa2c3207353ce4badda77
Mikhail Novikov <freiksenet@gmail.com>
2018-10-18 16:51:03 +0300

    Stitching

--- makeSchema.js
+++ makeSchema.js
@@ -1,5 +1,5 @@
 const schema = require("./schema");
-const { makeRemoteExecutableSchema, introspectSchema } = require('graphql-tools')
+const { makeRemoteExecutableSchema, introspectSchema, mergeSchemas, delegateToSchema, FilterRootFields, transformSchema } = require('graphql-tools')
 const { HttpLink } = require('apollo-link-http');
 const fetch = require('node-fetch')
 
@@ -7,5 +7,30 @@ module.exports = async function makeSchema() {
   const link = new HttpLink({ uri: 'https://api.graphcms.com/simple/v1/swapi', fetch})
   const clientSchema = await introspectSchema(link);
   const remoteSchema = makeRemoteExecutableSchema({ schema: clientSchema, link})
-  return remoteSchema;
+
+  const transformedSchema = transformSchema(remoteSchema, [new FilterRootFields((operation, fieldName) => fieldName === 'allFilms')]);
+
+  return mergeSchemas({
+    schemas: [transformedSchema, schema, `
+      extend type Post {
+        movie: Film!
+      }
+    `],
+    resolvers: {
+      Post: {
+        movie(parent, args, context, info) {
+          return delegateToSchema({
+            schema: remoteSchema,
+            operation: 'query',
+            fieldName: 'Film',
+            args: {
+              title: "The Empire Strikes Back"
+            },
+            context,
+            info,
+          })
+        }
+      }
+    }
+  });
 };
