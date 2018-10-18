e3f7e67cb66bd279cd04922ceb3f177cb9a91c6d
Mikhail Novikov <freiksenet@gmail.com>
2018-10-18 16:30:43 +0300

    Upd

--- makeSchema.js
+++ makeSchema.js
@@ -1,5 +1,11 @@
 const schema = require("./schema");
+const { makeRemoteExecutableSchema, introspectSchema } = require('graphql-tools')
+const { HttpLink } = require('apollo-link-http');
+const fetch = require('node-fetch')
 
 module.exports = async function makeSchema() {
-  return schema;
+  const link = new HttpLink({ uri: 'https://api.graphcms.com/simple/v1/swapi', fetch})
+  const clientSchema = await introspectSchema(link);
+  const remoteSchema = makeRemoteExecutableSchema({ schema: clientSchema, link})
+  return remoteSchema;
 };
